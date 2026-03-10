3d3cc1d19e3e83b51b2ecc8722c725f8d6e01ccd
sahebkumarghosh0452-lab <sahebkumarghosh0452@gmail.com>
2026-03-10 13:36:29 +0530

    Update script.js

--- Video 54/script.js
+++ Video 54/script.js
@@ -1,16 +1,22 @@
-alert("Hello World");
+// The server js that is runned by node . js  just dont have some browser specific version of js that is used or runned in brosers 
+alert("Hello World"); // Browser specific 
 
 console.log("Code is running...")
 console.log("Code is also running...")
 console.log("Code is looking like a wow...")
 
-var a = prompt("Enter your number")
-var isTrue = confirm("Are you sure you want to leave this page and blast your computer ")
+console.log(val1, /* …, */ valN) // To print same for other data type (except str)
+console.log(msg, subst1, /* …, */ substN) //To add subsequent strings in single static output 
+
+var a = prompt("Enter your number") // Give a section to enter a value in a  // Browser specific 
+
+var isTrue = confirm("Are you sure you want to leave this page and blast your computer ") // yeah ohk waala statement deta hai  // Browser specific 
+
 
 if(isTrue){
     console.log("Computer is blasting")
 }
-
+//Node js is a software made by an engineer who just take all elements from browers which used by js and compile them into a file node js  
 else{
     console.log("Computer is not blasting")
 
