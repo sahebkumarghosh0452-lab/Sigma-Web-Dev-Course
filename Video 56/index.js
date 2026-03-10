02c07f35afe0d4f1b0bb772060b67eafd7c851e3
sahebkumarghosh0452-lab <sahebkumarghosh0452@gmail.com>
2026-03-10 19:45:07 +0530

    Update index.js

--- Video 56/index.js
+++ Video 56/index.js
@@ -1,5 +1,6 @@
+//terminal can be opened as a program in vs code 
 console.log("Hello I am conditional tutorial")
-
+//null is def as obj as in 1995 the js is dev in a hurry so the obj and null have same tag just different pointers so its gaves the same name as obj 
 let age = 1;
 // let grace = 2;
 
@@ -9,7 +10,7 @@ let age = 1;
 // console.log(age - grace)
 // console.log(age * grace)
 // console.log(age / grace)
-// console.log(age ** grace)
+// console.log(age ** grace) - EXPONENTIAL operator 
 // console.log(age % grace)
 
 /*
@@ -18,10 +19,15 @@ multiline
 comment
 */
 
+// js can be run as rapple in powershell by using "node" cmd 
+
+// += , --, ++ , -= , /= , **= same as other prog lang 
+
 if (age == 18) {
     console.log("You can drive");
 }
-
+    // == - comapre only value not type e.g :- "3" == 3 - TRUE 
+// === - compare value and type 
 else if (age == 0) {
     console.log("Are you kidding?")
 }
