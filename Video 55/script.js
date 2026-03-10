61a7f35a4284289c60332904b772ae20aa18fae2
sahebkumarghosh0452-lab <sahebkumarghosh0452@gmail.com>
2026-03-10 19:27:48 +0530

    Update script.js

--- Video 55/script.js
+++ Video 55/script.js
@@ -1,8 +1,10 @@
 console.log("Hey this is tutorial 55");
-
-var a = 5;
+//var - updated and re-declared 
+var a = 5; // automatically detect the data type  - Global scope  
 // a = a + 1
-let b = 6;
+//Rules - letters , underscore and $ allowed for starting a var but digitts are not alllowed while starting a var
+//LET - updated but can't redeclared 
+let b = 6; //Local var initialized for particular block 
 let c = "Harry";
 let _a = "Shubham";
 // var 55a = "Rohan"; // Not Allowed
@@ -23,20 +25,20 @@ let y = 22;
 let z = 3.55;
 const p = true;
 let q = undefined;
-let r = null;
+let r = null; //OBJECT   //Due to stack overflow is depend on older code base where null is defined as obj so it can't be changed  
 
 console.log(x, y, z, p, q, r)
 console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
 
 
-let o = {
+let o = { //OBJ 
     "name": "Harry",
     "job code": 5600,
     "is_handsome": true
 }
 
 console.log(o);
-o.salary = "100crores";
+o.salary = "100crores"; //Addon statement in javascript 
 console.log(o);
 o.salary = "500crores";
 console.log(o);
