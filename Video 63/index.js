f31ad522ddc1dde338cf3fc1f32d19a827be36bb
SAHEB KUMAR GHOSH <sahebkumarghosh0452@gmail.com>
2026-03-11 12:03:24 +0530

    Refactor array examples and enhance comments
    Updated array manipulation examples and added comments.

--- Video 63/index.js
+++ Video 63/index.js
@@ -1,6 +1,9 @@
 let arr = [1, 2, 4, 5, 7]
 //  Index  0, 1, 2, 3, 4
 
+// arr in js can be of diff datatype under a single var 
+// arra are onj in js 
+
 arr[0] = 5666;
 // console.log(arr, typeof arr);
 // console.log(arr.length)
@@ -11,11 +14,20 @@ arr[0] = 5666;
 
 console.log(arr.toString())
 console.log(arr.join(" and "))
+a.pop() ; - take out the last ele of the arr 
+a.push(100) ; - add 100 at the end of the arr 
+a.shift() - remove an ele from begin of arr 
+a.unshift("Hello") ; - add Hello at the start of the arr 
+delete(a[2]) - asign undefined at a[2] 
+a1.concat(a2,a3)  ; - it add a2 and a3 and assign it to the a1 
+a.sort([7,8,8]) - modifies the original arr and sort the following 
+// sort also takes a compare function that can be used to return the sorted value according to the function 
+
 
 
  
 // let numbers = [1, 2, 3, 4, 5] 
-// numbers.splice(1, 2)    
-// numbers.splice(1, 3)  
-// numbers.splice(1, 3, 222, 333) 
-// (4) [1, 222, 333, 5]
+numbers.splice(1, 2)    
+numbers.splice(1, 3)  // return the deleted ele from arr from start and end index 
+numbers.splice(1, 3, 222, 333) 
+(4) [1, 222, 333, 5]
