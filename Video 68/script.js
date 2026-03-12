f4d4c0cc5f4b15498799b62edf2cc1bbbaabf7eb
SAHEB KUMAR GHOSH <sahebkumarghosh0452@gmail.com>
2026-03-12 19:15:43 +0000

    notes updated

--- Video 68/script.js
+++ Video 68/script.js
@@ -1,6 +1,6 @@
 console.log("Harry")
 
-// let boxes = document.getElementsByClassName("box")
+// let boxes = document.getElementsByClassName("box") - get the ele with a given "id" attribute  
 // console.log(boxes)
 
 // boxes[2].style.backgroundColor = "red"
@@ -8,8 +8,15 @@ console.log("Harry")
 // document.getElementById("redbox").style.backgroundColor = "red"
 
 // document.querySelector(".box").style.backgroundColor = "green";
-console.log(document.querySelectorAll(".box"))
+console.log(document.querySelectorAll(".box")) //- returns al ele inside as ele matching the given css selector 
 
 document.querySelectorAll(".box").forEach(e =>{
     e.style.backgroundColor = "green";
 }) 
+
+document.querySelector 
+let c = document.getElementsByTagName("div") //returns with the given tag name 
+document.getElementsByClassName
+c[0].matches
+c[0].closest
+c[0].contains  
